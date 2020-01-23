8f4c7b25e607642f00cb14bdb59c3fc03624b3e4
kei <kei.adc11.29@gmail.com>
2020-01-23 16:57:08 +0900

    formを実装

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,58 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+let datas = [];
+let counter = new Map();
+// key category
+
+//create data
+const category = ["IT", "WEB", "GAME", "OTHER"];
+const times = ["once", "twice", "thrice", "more"];
+const gender = ["man", "woman", "other"];
+const generation = ["eccStudent", "student", "teacher", "bender", "other"];
+
+for(let i =0; i < 10; i++){
+  let tmp = [category[Math.floor(Math.random()*10)%category.length], times[Math.floor(Math.random()*10)%times.length], gender[Math.floor(Math.random()*10)%gender.length], generation[Math.floor(Math.random()*10)%generation.length]];
+  let str = ["category", "times", "gender", "generation"];
+
+  datas.push([{
+    "category": tmp[0],
+    "times": tmp[1],
+    "gender": tmp[2],
+    "generation": tmp[3]
+  }]);
+
+  let map = counter.get(category[0]) || new Map();
+  if(map.has(tmp[0])){
+    map.set(tmp[0], map.get(tmp[0]+1))
+  }
+  map.set(tmp[0], 1);
+
+  for(let i = 0; i < tmp.length; i++){
+    
+  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+
+//GET form page
+router.get('/form', (req, res, next) => {
+  res.render('form');
+});
+
+router.post('/form', (req, res, next) => {
+  datas.push(req.body);
+  res.redirect('/form');
+
+});
+
+router.get('/api/v1/', (req, res, next) => {
+  res.json({
+    datas
+  });
 });
 
 module.exports = router;
